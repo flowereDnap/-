272817e45af2180267b1337593c7fc5457811557
flowereDnap <nikitashapko5@gmail.com>
2022-07-18 16:50:01 +0300

    second

--- ??/ViewController.swift
+++ ??/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    self.title = "Home"
+    
+    let menuBtn: UIButton = UIButton(frame: CGRect(x: 0.0, y: 0.0, width: 6.0, height: 8.0))
+          menuBtn.setImage(UIImage(named: "logo")!, for: .normal)
+          menuBtn.addTarget(self, action: #selector(sample), for: .touchUpInside)
+    self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: menuBtn)
+    
+    let sampleBtn: UIButton = UIButton(frame: CGRect(x: 0.0, y: 0.0, width: 34.0, height: 44.0))
+    sampleBtn.setImage(UIImage(named: "svgSample")!, for: .normal)
+    sampleBtn.addTarget(self, action: #selector(sample), for: .touchUpInside)
+    self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: sampleBtn)
   }
 
-
+  @objc func sample(_ sender: UIButton) {
+    
+  }
 }
 
